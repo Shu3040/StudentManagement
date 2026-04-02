5f2d0aef1dcdb21605850d52aaa197340df71d53
Shu3040 <157202675+Shu3040@users.noreply.github.com>
2026-04-02 20:59:51 +0900

    mysqlと連動して受講生情報の検索を行う機能を追加

---
+++ src/main/java/raisetech/student/managemant/StudentRepository.java
@@ -0,0 +1,17 @@
+package raisetech.student.managemant;
+
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+
+@Mapper
+public interface StudentRepository {
+
+    @Select("SELECT * FROM students")
+    List<Student> search();
+
+
+
+}
+
