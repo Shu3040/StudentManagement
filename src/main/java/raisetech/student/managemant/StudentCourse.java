bc22b314baa41f70b1b1f947319e43152b879f8c
Shu3040 <157202675+Shu3040@users.noreply.github.com>
2026-04-02 21:42:31 +0900

    mysqlから受講コース情報をとってくる機能を追加

---
+++ src/main/java/raisetech/student/managemant/StudentCourse.java
@@ -0,0 +1,16 @@
+package raisetech.student.managemant;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class StudentCourse {
+
+    private  String courseid;
+    private  String id;
+    private  String coursename;
+    private  String startdate;
+    private  String enddate;
+}
