263dbef6e06f979cc82e47b04188e9d63728cf01
Shu3040 <157202675+Shu3040@users.noreply.github.com>
2026-04-06 15:35:20 +0900

    コントローラーを作成したHTMLで出力するように変更

---
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -0,0 +1,49 @@
+package raisetech.student.management.controller;
+
+import jakarta.annotation.Nonnull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import raisetech.student.management.controller.converter.StudentConverter;
+import raisetech.student.management.data.Student;
+import raisetech.student.management.data.StudentCourses;
+import raisetech.student.management.domain.StudentDetail;
+import raisetech.student.management.service.StudentService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+public class StudentController {
+
+    private StudentService service;
+    private StudentConverter converter;
+
+    @Autowired
+    public StudentController(StudentService service, StudentConverter converter) {
+        this.service = service;
+        this.converter = converter;
+    }
+
+
+
+    @GetMapping("/studentList")
+    public String getStudentList(Model model) {
+        List<Student> students = service.searchStudentList();
+        List<StudentCourses> studentCourses = service.searchStudentCourseList();
+
+        model.addAttribute("studentList",converter.convertStudentDetails(students,studentCourses));
+        return "studentList";
+    }
+
+
+
+    @GetMapping("/studentCourseList")
+    public List<StudentCourses> getStudentCourseList() {
+        return service.searchStudentCourseList();
+    }
+}
+
