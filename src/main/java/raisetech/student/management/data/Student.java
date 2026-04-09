acc6ddd4e87dee074ef84e96526b95cc896858b8
Shu3040 <157202675+Shu3040@users.noreply.github.com>
2026-04-09 11:03:08 +0900

    データパッケージが消えたため復元

---
+++ src/main/java/raisetech/student/management/data/Student.java
@@ -0,0 +1,22 @@
+package raisetech.student.management.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student {
+
+    private Integer id;
+    private String name;
+    private String furigana;
+    private String nickname;
+    private String mailaddress;
+    private String address;
+    private String tel;
+    private int age;
+    private String gender;
+    private String remark;
+    private boolean isDeleted;
+
+}
