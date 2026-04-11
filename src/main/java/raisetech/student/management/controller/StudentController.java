4dc6857b43757ed821e64f5124d784ed1d4d8f13
Shu3040 <157202675+Shu3040@users.noreply.github.com>
2026-04-11 17:48:27 +0900

    アップデート処理の呼び出しを追加

--- src/main/java/raisetech/student/management/controller/StudentController.java
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import raisetech.student.management.controller.converter.StudentConverter;
 import raisetech.student.management.data.Student;
@@ -13,6 +14,7 @@ import raisetech.student.management.data.StudentCourse;
 import raisetech.student.management.domain.StudentDetail;
 import raisetech.student.management.service.StudentService;
 
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -39,25 +41,29 @@ public class StudentController {
         return "studentList";
     }
 
+    @GetMapping("/Student/{id}")
+    public String getStudent(@PathVariable String id, Model model) {
+        StudentDetail studentDetail =service.serchStudent(id);
+        model.addAttribute("studentDetail", studentDetail);
+        return "updateStudent";
+    }
 
 
-    @GetMapping("/studentCourseList")
+    /*@GetMapping("/studentCourseList")
     public String getStudentCourseList(Model model) {
         List<StudentCourse> studentCourses = service.searchStudentCourseList();
 
         model.addAttribute("studentCourseList",studentCourses);
         return "studentCourseList";
-    }
+    }*/
 
     @GetMapping("/newStudent")
     public String newStudent(Model model){
         StudentDetail studentDetail = new StudentDetail();
-        studentDetail.setStudent(new Student());
-        model.addAttribute("studentDetail", new StudentDetail());
+        studentDetail.setStudentCourse(Arrays.asList(new StudentCourse()));
+        model.addAttribute("studentDetail", studentDetail);
         return "registerStudent";
     }
-
-
 
     @PostMapping ("/registerStudent")
     public  String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result){
@@ -65,10 +71,21 @@ public class StudentController {
             return "registerStudent";
         }
 
-        service.registerStudent(studentDetail.getStudent());
+        //service.registerStudent(studentDetail.getStudent());
+        service.registerStudent(studentDetail);
 
         System.out.println(studentDetail.getStudent().getName()+"さんが新規受講生として登録されました。");
     return "redirect:/studentList";
     }
+
+    @PostMapping ("/updateStudent")
+    public  String updateStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+        if (result.hasErrors()) {
+            return "updateStudent";
+        }
+        service.updateStudent(studentDetail);
+        return "redirect:/studentList";
+    }
 }
+
 
