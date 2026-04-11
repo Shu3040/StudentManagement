79be44347fbeaaba21a7bdc13b1fd48662313d8a
Shu3040 <157202675+Shu3040@users.noreply.github.com>
2026-04-11 17:49:50 +0900

    アップデート処理を追加

--- src/main/java/raisetech/student/management/repository/StudentRepository.java
+++ src/main/java/raisetech/student/management/repository/StudentRepository.java
@@ -11,12 +11,35 @@ import java.util.List;
 public interface StudentRepository {
 
     @Select("SELECT * FROM students")
-    List<Student> searchStudent();
+    List<Student> search();
+
+    @Select("SELECT * FROM students WHERE id = #{id}")
+    Student searchStudent(String id);
+
+    @Select("SELECT * FROM students_courses")
+    List<StudentCourse> searchStudentCoursesList();
+
+    @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
+    List<StudentCourse> searchStudentsCourses(String studentid);
+
 
     @Insert("INSERT INTO students(name, furigana, nickname, mailaddress, address, tel, age, gender, remark, is_deleted) " +
             "VALUES(#{name}, #{furigana}, #{nickname}, #{mailaddress}, #{address}, #{tel}, #{age}, #{gender}, #{remark}, #{isDeleted})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    void insertStudent(Student student);
+    void registerStudent(Student student);
+
+@Insert("INSERT INTO students_courses(student_id, course_name, start_date, end_date) " +
+        "VALUES(#{studentid}, #{coursename}, #{startdate}, #{enddate})")
+@Options(useGeneratedKeys = true, keyProperty = "id")
+    void registerStudentCourse(StudentCourse studentCourse);
+
+    @Update("UPDATE students SET (name = #{name}, furigana = #{furigana}, nickname = #{nickname}, mailaddress = #{mailaddress}," +
+            " address = #{address}, tel = #{tel}, age = #{age}, gender = #{gender},remark = #{remark}, isDeleted = #{isDeleted}) WHERE id = #{id}")
+    void updateStudent(Student student);
+
+    @Update("UPDATE students_courses SET(course_name = #{coursename} WHERE id = #{id}")
+    void updateStudentCourse(StudentCourse studentCourse);
+
 
     @Select("SELECT * FROM students_courses")
     List<StudentCourse> searchStudentCourses();
