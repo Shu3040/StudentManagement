2b145866262324bbc58294452c047ad84091f2af
Shu3040 <157202675+Shu3040@users.noreply.github.com>
2026-04-11 17:47:55 +0900

    アップデート処理を追加

--- src/main/java/raisetech/student/management/service/StudentService.java
+++ src/main/java/raisetech/student/management/service/StudentService.java
@@ -2,10 +2,13 @@ package raisetech.student.management.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import raisetech.student.management.data.Student;
 import raisetech.student.management.data.StudentCourse;
+import raisetech.student.management.domain.StudentDetail;
 import raisetech.student.management.repository.StudentRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -20,14 +23,43 @@ public class StudentService {
 
     //機能を追加したいときに処理（コード）をわかりやすくするためにコントローラーではなくサービスのクラスを作成して処理を行う。
     public List<Student> searchStudentList() {
-        return repository.searchStudent();
+        return repository.search();
+    }
+
+    public StudentDetail serchStudent(String id){
+        Student student = repository.searchStudent(id);
+        List<StudentCourse> studentCourses = repository.searchStudentsCourses(student.getId());
+        StudentDetail studnetDetail = new StudentDetail();
+        studnetDetail.setStudent(student);
+        studnetDetail.setStudentCourse(studentCourses);
+        return studnetDetail;
     }
 
     public List<StudentCourse> searchStudentCourseList() {
-        return repository.searchStudentCourses();
+        return repository.searchStudentCoursesList();
     }
 
-        public void registerStudent(Student student) {
-            repository.insertStudent(student);
+
+@Transactional
+      //  public void registerStudent(Student student) {
+            public void registerStudent(StudentDetail studentDetail) {
+    //       repository.insertStudent(student);
+    repository.registerStudent(studentDetail.getStudent());
+
+    for (StudentCourse studentCourse : studentDetail.getStudentCourse()) {
+        studentCourse.setStudentId(studentDetail.getStudent().getId());
+        studentCourse.setStartDate(LocalDateTime.now());
+        studentCourse.setEndDate(LocalDateTime.now().plusYears(1));
+
+        repository.registerStudentCourse(studentCourse);
     }
+}
+
+    @Transactional
+    public void updateStudent(StudentDetail studentDetail) {
+        repository.updateStudent(studentDetail.getStudent());
+        for (StudentCourse studentCourse : studentDetail.getStudentCourse()){
+            repository.updateStudentCourse(studentCourse);
+        }
+}
 }
