de0859734cd34734ddca99d0a056d7efa01983b3
Shu3040 <157202675+Shu3040@users.noreply.github.com>
2026-04-12 14:35:29 +0900

    削除処理の修正

--- src/main/java/raisetech/student/management/repository/StudentRepository.java
+++ src/main/java/raisetech/student/management/repository/StudentRepository.java
@@ -10,11 +10,12 @@ import java.util.List;
 @Mapper
 public interface StudentRepository {
 
-    @Select("SELECT * FROM students")
+    @Select("SELECT * FROM students" )
     List<Student> search();
 
     @Select("SELECT * FROM students WHERE id = #{id}")
     Student searchStudent(String id);
+
 
     @Select("SELECT * FROM students_courses")
     List<StudentCourse> searchStudentCoursesList();
@@ -28,18 +29,20 @@ public interface StudentRepository {
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void registerStudent(Student student);
 
-@Insert("INSERT INTO students_courses(student_id, course_name, start_date, end_date) " +
-        "VALUES(#{studentid}, #{coursename}, #{startdate}, #{enddate})")
-@Options(useGeneratedKeys = true, keyProperty = "id")
+    @Insert("INSERT INTO students_courses(student_id, course_name, start_date, end_date) " +
+        "VALUES(#{studentId}, #{courseName}, #{startDate}, #{endDate})")
+    @Options(useGeneratedKeys = true, keyProperty = "courseId")
     void registerStudentCourse(StudentCourse studentCourse);
 
-    @Update("UPDATE students SET (name = #{name}, furigana = #{furigana}, nickname = #{nickname}, mailaddress = #{mailaddress}," +
-            " address = #{address}, tel = #{tel}, age = #{age}, gender = #{gender},remark = #{remark}, isDeleted = #{isDeleted}) WHERE id = #{id}")
+    @Update("UPDATE students SET name = #{name}, furigana = #{furigana}, nickname = #{nickname}, mailaddress = #{mailaddress}," +
+            " address = #{address}, tel = #{tel}, age = #{age}, gender = #{gender},remark = #{remark}, is_deleted = #{isDeleted} WHERE id = #{id}")
     void updateStudent(Student student);
 
-    @Update("UPDATE students_courses SET(course_name = #{coursename} WHERE id = #{id}")
-    void updateStudentCourse(StudentCourse studentCourse);
 
+    @Update("UPDATE students_courses SET course_name = #{courseName} WHERE student_id = #{studentId} AND course_id = #{courseId}")
+    void updateStudentCourse(StudentCourse studentCourse);
+    /*@Update("UPDATE students_courses SET course_name = #{courseName} WHERE id = #{id}")
+    void updateStudentCourse(StudentCourse studentCourse);*/
 
     @Select("SELECT * FROM students_courses")
     List<StudentCourse> searchStudentCourses();
