43ca6394e60bd412bc7eaf5e930a7fdcfa5dc6d4
Shu3040 <157202675+Shu3040@users.noreply.github.com>
2026-04-15 18:06:45 +0900

    StudentRepositoryのxmlを作成し、内容を整理

--- src/main/java/raisetech/student/management/repository/StudentRepository.java
+++ src/main/java/raisetech/student/management/repository/StudentRepository.java
@@ -6,73 +6,60 @@ import raisetech.student.management.data.StudentCourse;
 
 import java.util.List;
 
-/*
+    /*
     受講生テーブルと受講生コーステーブルと紐づくRepositoryです
      */
 @Mapper
 public interface StudentRepository {
     /*
-        受講生全件検索を行います
-        @return 受講生一覧（全件）
-         */
-    @Select("SELECT * FROM students" )
+    受講生全件検索を行います
+    @return 受講生一覧（全件）
+     */
     List<Student> search();
     /*
-       受講生検索を行います
-       @parm　id 受講生ID
-       @return 受講生情報
+    受講生検索を行います
+    @parm　id 受講生ID
+    @return 受講生情報
      */
-    @Select("SELECT * FROM students WHERE id = #{id}")
     Student searchStudent(String id);
 
     /*
-       受講生のコース情報の全件検索を行います
-       @parm　id 受講生ID
-       @return 受講生のコース情報（全件）
-                */
-    @Select("SELECT * FROM students_courses")
+    受講生のコース情報の全件検索を行います
+    @parm　id 受講生ID
+    @return 受講生のコース情報（全件）
+     */
     List<StudentCourse> searchStudentCoursesList();
 
     /*
-          受講生IDに紐づくコース情報の検索を行います
-          @parm　studentId 受講生ID
-          @return 受講生IDに紐づく受講生のコース情報
-                   */
-    @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
+    受講生IDに紐づくコース情報の検索を行います
+    @parm　studentId 受講生ID
+    @return 受講生IDに紐づく受講生のコース情報
+     */
     List<StudentCourse> searchStudentCourse(String studentid);
 
     /*
-             受講生を新規登録します
-             IDに関しては自動採番を行う
-             @parm　student　受講生
-                      */
-    @Insert("INSERT INTO students(name, furigana, nickname, mailaddress, address, tel, age, gender, remark, is_deleted) " +
-            "VALUES(#{name}, #{furigana}, #{nickname}, #{mailaddress}, #{address}, #{tel}, #{age}, #{gender}, #{remark}, #{isDeleted})")
-    @Options(useGeneratedKeys = true, keyProperty = "id")
+    受講生を新規登録します
+    IDに関しては自動採番を行う
+    @parm　student　受講生
+     */
     void registerStudent(Student student);
 
     /*
-                受講生コース情報を新規登録します
-                @parm　studentCourse　受講生コース情報
-                         */
-    @Insert("INSERT INTO students_courses(student_id, course_name, start_date, end_date) " +
-        "VALUES(#{studentId}, #{courseName}, #{startDate}, #{endDate})")
-    @Options(useGeneratedKeys = true, keyProperty = "courseId")
+    受講生コース情報を新規登録します
+    @parm　studentCourse　受講生コース情報
+     */
     void registerStudentCourse(StudentCourse studentCourse);
 
     /*
-               受講生詳細の更新を行います
-               @parm　student　受講生
-                        */
-    @Update("UPDATE students SET name = #{name}, furigana = #{furigana}, nickname = #{nickname}, mailaddress = #{mailaddress}," +
-            " address = #{address}, tel = #{tel}, age = #{age}, gender = #{gender},remark = #{remark}, is_deleted = #{isDeleted} WHERE id = #{id}")
+    受講生詳細の更新を行います
+    @parm　student　受講生
+     */
     void updateStudent(Student student);
 
     /*
-     受講生コース情報のコース名を更新します
-   　 @parm　studentCourse　受講生コース情報
+    受講生コース情報のコース名を更新します
+    @parm　studentCourse　受講生コース情報
      */
-    @Update("UPDATE students_courses SET course_name = #{courseName} WHERE student_id = #{studentId} AND course_id = #{courseId}")
     void updateStudentCourse(StudentCourse studentCourse);
 
 }
