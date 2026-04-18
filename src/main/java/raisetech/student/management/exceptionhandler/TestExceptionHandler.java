1317e80c8652fcc8f61645005f453f24d0218ee1
Shu3040 <157202675+Shu3040@users.noreply.github.com>
2026-04-18 17:31:15 +0900

    TestExceptionHandlerのコードを修正

--- src/main/java/raisetech/student/management/exceptionhandler/TestExceptionHandler.java
+++ src/main/java/raisetech/student/management/exceptionhandler/TestExceptionHandler.java
@@ -3,10 +3,12 @@ package raisetech.student.management.exceptionhandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import raisetech.student.management.exception.TestException;
 
-public class TestExceptionHandler extends Exception{
-    @ExceptionHandler(TestExceptionHandler.class)
+@RestControllerAdvice
+public class TestExceptionHandler {
+    @ExceptionHandler(TestException.class)
     public ResponseEntity<String> handleTestException(TestException ex){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
