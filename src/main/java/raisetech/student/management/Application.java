3aff50d718737e9ecf263ec9979ffdd86f961c9f
Shu3040 <157202675+Shu3040@users.noreply.github.com>
2026-04-20 19:01:42 +0900

    不要なコードがあったため削除

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,35 +1,21 @@
 package raisetech.student.management;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
+
+@OpenAPIDefinition(info = @Info(title = "受講生管理システム"))
 @SpringBootApplication
-@RestController
 public class Application {
-
-	@Autowired
-	private StudentRepository repository;
-
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(Application.class, args);
 	}
-
-	@GetMapping("/studentList")
-	public List<Student> getStudentList() {
-		return repository.searchStudent();
 	}
 
-	@GetMapping("/studentCourseList")
-	public List<StudentCourse> getStudentCourseList() {
-		return repository.searchStudentCourses();
-	}
-}
 
 
 
