fcac5373a3bc6a73a0aae05f970e6c8f7d3e603b
Shu3040 <157202675+Shu3040@users.noreply.github.com>
2026-04-20 18:40:16 +0900

    mainのApplicationクラスにOpenAPI Generatorを適用

--- src/test/java/raisetech/student/management/Application.java
+++ src/test/java/raisetech/student/management/Application.java
@@ -2,20 +2,14 @@ package raisetech.student.management;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
 
+
+@SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "受講生管理システム"))
-@SpringBootTest
-class ApplicationTests {
-	public static void main(String[] args){
-		SpringApplication.run(Application.class,args);
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
 	}
-
-	@Test
-	void contextLoads() {
 	}
-
-}
