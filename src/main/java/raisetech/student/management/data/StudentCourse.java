7fa169b7c25925cf3879a8daadfb0b4da661a03d
Shu3040 <157202675+Shu3040@users.noreply.github.com>
2026-04-20 16:49:11 +0900

    スキーマの実装

--- src/main/java/raisetech/student/management/data/StudentCourse.java
+++ src/main/java/raisetech/student/management/data/StudentCourse.java
@@ -1,10 +1,12 @@
 package raisetech.student.management.data;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Schema(description = "受講生コース情報")
 @Getter
 @Setter
 
